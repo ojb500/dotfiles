1f63df65477491d258cf17a2049fb2e777293718
ojb500 <ojb500@users.noreply.github.com>
2022-05-18 16:01:07 +0100

    add profile

---
+++ ojb-profile.ps1
@@ -0,0 +1,26 @@
+﻿# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+
+function Push-NewGitRemoteBranch()
+{
+	$Branch = git rev-parse --abbrev-ref HEAD
+	if (-not $?) {
+		throw "Couldn't get current branch"
+	}
+	$Branch = $Branch.Trim()
+	$BranchSpec = $Branch + ":users/oli/$Branch"
+	echo "pushing $BranchSpec"
+	git push -u origin $BranchSpec
+    start $(git remote get-url origin)
+}
+
+# Import-Module oh-my-posh
+Import-Module posh-git
+$GitPromptSettings.EnablePromptStatus = $false
+
+$env:POSH_GIT_ENABLED = $True
+oh-my-posh init pwsh --config "$PSScriptRoot\tiwahu-ojb.omp.json" | Invoke-Expression
